6625f09411ac86cb3da8db0c1f458f6cf21b0d43
Purple19-dev <purplebird200126@gmail.com>
2023-03-16 07:38:14 -0700

    update commit

--- js/app.js
+++ js/app.js
@@ -1,104 +1,56 @@
 /**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
  * Define Global Variables
- *
  */
-
 const navList = document.querySelector("#navbar__list");
 const sections = document.querySelectorAll("section");
 
 /**
- * End Global Variables
- * Start Helper Functions
- *
+ * Helper Function
  */
-
-function inVeiwport(params) {
-  const sectionBond = params.getBoundingClientRect();
+function isInViewport(elem) {
+  const bounding = elem.getBoundingClientRect();
   return (
-    sectionBond.top >= 0 &&
-    sectionBond.left >= 0 &&
-    sectionBond.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    sectionBond.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
 /**
- * End Helper Functions
- * Begin Main Functions
- *
+ * Main Functions
  */
+function buildNav() {
+  for (let section of sections) {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    const navText = section.getAttribute("data-nav");
+    link.href = `#${section.id}`;
+    link.innerHTML = navText;
+    link.classList.add("menu__link");
+    listItem.appendChild(link);
+    navList.appendChild(listItem);
 
-// build the nav
-function navBar() {
-  sections.forEach((section) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const navText = section.dataset.nav;
-    a.href = `#${section.id}`;
-    a.textContent = navText;
-    a.className = "menu__link";
-    li.appendChild(a);
-    //scrollSections(li, section);
-    navList.appendChild(li);
-  });
+    // Scroll to section on link click
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  }
 }
 
-// Add class 'active' to section when near top of viewport
-function setSectionActive() {
-  sections.forEach((section) => {
-    if (inVeiwport(section)) {
+function setActive() {
+  for (let section of sections) {
+    if (isInViewport(section)) {
       section.classList.add("your-active-class");
     } else {
       section.classList.remove("your-active-class");
     }
-  });
-}
-
-// Scroll to anchor ID using scrollTO event
-function scrollSections(section) {
-  li.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo({
-      top: section.offsetTop,
-      behavior: "smooth",
-    });
-  });
+  }
 }
 
 /**
- * End Main Functions
- * Begin Events
- *
+ * Events
  */
-
-// Build menu
-navBar();
-
-// Scroll to section on link click
-
-// Set sections as active
-window.addEventListener("scroll", (event) => {
-  setSectionActive();
-});
+document.addEventListener("DOMContentLoaded", buildNav);
+window.addEventListener("scroll", setActive);
